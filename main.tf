68b44425c3ea6ebe813dd4497d31a8c736e7ce36
Theo Pack Test <tf.pack@gmail.com>
2016-03-20 18:35:33 +0100

    Add terraform files

---
+++ main.tf
@@ -0,0 +1,23 @@
+provider "digitalocean" {  
+    token = "${var.do_token}"
+}
+
+resource "digitalocean_droplet" "docker" {  
+    image = "docker"
+    name = "docker"
+    region = "fra1"
+    size = "1gb"
+    ssh_keys = [
+    	"${var.ssh_fingerprint}"
+    ]
+    connection {
+        user = "root"
+        key_file = "${var.key_path}"
+    }
+    provisioner "remote-exec" {
+        inline = [
+           "docker run -d --name world furikuri/terra-world",
+           "docker run -d -p 8080:8080 --name hello --link world:world furikuri/terra-world",
+        ]
+    }
+}
